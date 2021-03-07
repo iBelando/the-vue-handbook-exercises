16a49b822f9f1fcde95b54e8b2b0b745619371ff
iBelando <ignaciobelando@gmail.com>
2021-03-07 00:52:39 -0300

    10. Methods

---
+++ 10. methods/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <!-- How to access data from a method -->
+    <a @click="handleClick()">Click me!</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Nacho'
+    }
+  },
+  methods: {
+    handleClick: function() {
+      //  We don't have to use this.data.name. It'll raise an error
+      console.log(this.name);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
