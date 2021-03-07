7f06d5ac3fc6cd5fb8ed16e9c2b712cb41ec6688
iBelando <ignaciobelando@gmail.com>
2021-03-07 00:53:41 -0300

    11. Watchers

---
+++ 11. watchers/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <p>My name is {{ name }}</p>
+    <button @click="changeName()">Change my name!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Nacho'
+    }
+  },
+  methods: {
+    changeName: function() {
+      this.name = 'Nachus'
+    }
+  },
+  watch: {
+    name: function(newValue, oldValue) {
+      console.log(newValue, oldValue);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
