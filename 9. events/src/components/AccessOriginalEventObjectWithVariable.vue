cdaca5fdc92c8a5239d6d4647a5cf468a901e922
iBelando <ignaciobelando@gmail.com>
2021-03-07 00:13:08 -0300

    9. Events

---
+++ 9. events/src/components/AccessOriginalEventObjectWithVariable.vue
@@ -0,0 +1,15 @@
+<template>
+    <!-- If you already pass a variable -->
+    <a @click="handleClick('something', $event)">Click me!</a>
+</template>
+
+<script>
+export default {
+    methods: {
+        handleClick: function (text, event) {
+            console.log(text);
+            console.log(event);
+        }
+    }
+}
+</script>
